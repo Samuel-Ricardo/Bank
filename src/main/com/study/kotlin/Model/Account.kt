837026fadbb0a53ac83e763aaae1ebc613158372
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-07-23 20:15:15 -0300

    override: Function - withdraw() - (Model.Account)

--- src/main/com/study/kotlin/Model/Account.kt
+++ src/main/com/study/kotlin/Model/Account.kt
@@ -1,11 +1,11 @@
 package com.study.kotlin.Model;
 
-
+import com.study.kotlin.Interface.BankTransition;
 
 abstract class Account(
   open val number:Int,
   open val agency:String,
-  private var balance: Double = 0.0){
+  private var balance: Double = 0.0): BankTransition {
 
   abstract fun rate(): Double
 
@@ -17,5 +17,23 @@ abstract class Account(
     return balance;
   }
 
+  override fun withdraw(value:Double){
+
+    if(checkBalance(value)) {
+      balance -= value;
+
+      println("")
+      println("- Saque realizado com sucesso")
+      println("- ")
+      println("- Saldo Atual: $balance")
+      println("")
+    } else {
+
+      println("")
+      println("- Saldo Insuficiente :(")
+      println("- Faltam ${balance - value}")
+      println("")
+    }
+  }
 
 }
