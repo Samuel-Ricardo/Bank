1a3c933d5003c352aee4f3cdf16a697963e72b43
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-07-23 19:01:57 -0300

    created user interface

--- src/main/com/study/kotlin/app.kt
+++ src/main/com/study/kotlin/app.kt
@@ -1,4 +1,40 @@
+import java.util.Scanner
+
 fun main() {
 
+  var scanner = Scanner(System.`in`);
+  var online = true;
+
+  println("")
   println("Welcome to Bank :)")
+
+
+  while(online){
+
+    println("")
+    println("Qual Operação deseja realizar?")
+    println("")
+    println("===============================")
+    println("")
+    println("(1) - Logar na minha conta")
+    println("(2) - Sair")
+    println("")
+    println("================================")
+    println("")
+
+    var option = scanner.nextInt();
+
+    when(option){
+
+      1 -> println("Serviço Indisponível");
+      2 -> {
+        online = false
+        println("")
+      }
+
+      else -> { // Note the block
+
+      }
+    }
+  }
 }
