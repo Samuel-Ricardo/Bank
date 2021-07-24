d4c9f2ee72fa203326a33ee47f70a029dacfd3fe
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-07-24 13:13:35 -0300

    to string

--- src/main/com/study/kotlin/Model/Client/PessoaFisica.kt
+++ src/main/com/study/kotlin/Model/Client/PessoaFisica.kt
@@ -6,11 +6,27 @@ class PessoaFisica(
   val name:String,
   val age:Int,
   override var address:Address,
-  val CPF:String = "",
+  val CPF:String,
   override var account:Account
 ):Client(address, account) {
 
   override fun printClientData(){
 
+    println("""
+      =============================================
+      - Pessoa Física
+      ---------------------------------------------
+      - Nome: ${name}
+      - Idade: ${age}
+      - CPF: ${CPF}
+      ---------------------------------------------
+      - Endereço: ${this.address.city}
+      - CEP: ${this.address.cep}
+      ---------------------------------------------
+      - Conta: ${account.getNumber()}
+      - Saldo Atual: R$ ${account.getBalance()}
+      - Agencia: ${this.account.getAgency()}
+      =============================================
+    """.trimIndent())
   }
 }
