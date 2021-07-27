aefc41d3310aa9dc1247ad83054b2c3d17e68d73
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-07-27 14:40:55 -0300

    Create: Function - insertPessoaJuridica() - (API.Accounts)

--- src/main/com/study/kotlin/API/Accounts.kt
+++ src/main/com/study/kotlin/API/Accounts.kt
@@ -4,6 +4,7 @@ import com.study.kotlin.Model.Account;
 import com.study.kotlin.Model.Client.PessoaFisica;
 import com.study.kotlin.Model.Client.Client;
 import com.study.kotlin.Model.Client.Address;
+import com.study.kotlin.Model.Client.PessoaJuridica
 
 import kotlin.random.Random;
 import com.study.kotlin.App
@@ -14,7 +15,7 @@ class Accounts{
 
   private fun generateId():Int{
 
-    return Random.nextInt(0, Random.nextInt(0, 10000));
+    return Random.nextInt(0, Random.nextInt(0, 1000000));
   }
 
   fun insertPessoaFisica(account:Account, name:String, age:Int, address:Address, cpf:String):PessoaFisica{
@@ -31,6 +32,27 @@ class Accounts{
     accounts.put(pf.id, pf);
 
     return  accounts.get(pf.id) as PessoaFisica;
+  }
+
+  fun insertPessoaJuridica(
+          account:Account,
+          name:String,
+          CNPJ:String,
+          reason:String,
+          address:Address):PessoaJuridica{
+
+    var pj:PessoaJuridica = PessoaJuridica(
+      generateId(),
+      name = name,
+      account = account,
+      address = address,
+      pCNPJ = CNPJ,
+      socialReason = reason,
+    )
+
+    accounts.put(pj.id, pj);
+
+    return accounts.get(pj.id) as PessoaJuridica;
   }
 
 
